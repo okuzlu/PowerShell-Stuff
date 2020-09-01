db88f3ce3820614dcc15d675a2dc400ace1007ea
Onur Kuzlu <onur.kuzlu@th-koeln.de>
2020-09-01 13:46:56 +0200

    Update Robocopy-Function.ps1
    added examples

--- Robocopy-Function.ps1
+++ Robocopy-Function.ps1
@@ -10,3 +10,14 @@ function Copy-Files {
     )
     Robocopy.exe $Path $Destination $File $Arg
 }
+
+<# Examples
+
+Copy cmd.exe to $InstallPath: 
+Copy-Files -Path "C:\Windows\System32\" -File "cmd.exe"
+
+Copy (Mirror) C:\Users Directory to $InstallPath\UserData: 
+Copy-Files -Path "C:\Users\" -Destination "$InstallPath\UserData" -Arg "/MIR"
+
+#>
+
